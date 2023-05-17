7d7b75bdd0042a01bf1f00d2d4a76e5449632f1a
Marcelo Rodrigues <manoelmarcelo.r@gmail.com>
2023-05-17 10:16:29 -0300

    organiza

--- Pokemon/src/components/cardPokemons/index.jsx
+++ Pokemon/src/components/cardPokemons/index.jsx
@@ -1,48 +1,99 @@
-import { Box, Flex, Heading, Image, LinkBox, LinkOverlay, Text } from '@chakra-ui/react'
-import React from 'react'
+import {
+  Box,
+  Flex,
+  Heading,
+  Image,
+  LinkBox,
+  LinkOverlay,
+  Text,
+} from "@chakra-ui/react";
+import React from "react";
 
-
-const CardPokemons = ({name, imagem, types}) => {
-    
-    const typepoke = () => {
-        if(types[1]){
-            return types[0].type.name+" / "+types[1].type.name
-        }
-        return types[0].type.name
+const CardPokemons = ({ name, imagem, types }) => {
+  const typepoke = () => {
+    if (types[1]) {
+      return types[0].type.name + "  /  " + types[1].type.name;
     }
+    return types[0].type.name;
+  };
 
   return (
     <>
-        <LinkBox boxShadow="0 0 10px rgba(0,0,0,.15)" height="195" position="relative" p="10px 10px 10px 30px" css={{  'box-shadow': '0 0 10px rgba(0,0,0,.15)' }} bg="#fff" borderRadius="20"
-               _before={{ top: '10px', left: '12px', position: 'absolute', content: '" "', display: 'block', width: '5px', height: '180px', background: 'green', 'border-radius': '5px' }}>
-              <Flex display={'flex'} gap={'10px'}>
-                <Image borderRadius="5px" border="1px solid #dfdfdf" height="100px" width="120px" src={imagem}
-                  alt='Image' display="inline-block"/>
-                <Heading fontSize="15px" lineHeight="20px" css={{ display: '-webkit-box', 'max-width': '100%', '-webkit-line-clamp': '5', '-webkit-box-orient': 'vertical', 'text-overflow': 'ellipsis', overflow: 'hidden' }} height="100px" as="h3">
-                    <LinkOverlay href='/'>
-                        {name}
-                    </LinkOverlay>
-                </Heading>
-              </Flex>
-              <Flex display={'flex'} gap={'10px'} mt={'12px'}>
-                <Box fontSize={'12px'} height="65px" lineHeight="65px" w="90px" bg="#CCF0D3" color="#07721A" textAlign="center" borderRadius={'10px'}>
-                    <Text as="span" css={{ display: 'inline-block', 'vertical-align': 'middle', 'line-height': 'normal' }}>
-                        
-                    </Text>
-                </Box>
-                <Box textAlign="center" w={'100%'}>
-                  <Box height="40px" lineHeight="40px" bg="#F0F0F0" textAlign="center" w={'100%'} borderRadius="10px 10px 0 0" fontWeight="bold" fontSize={'12px'}>
-                     {typepoke()}
-                  </Box>
-
-                  <Box mt="2px" fontSize="13px" height="23px" lineHeight="23px" fontWeight="bold" bg={'blue'} color="#DD0000" borderRadius="0 0 10px 10px">
-                    
-                  </Box>
-                </Box>
-              </Flex> 
-            </LinkBox>
+      <LinkBox
+        boxShadow="0 0 10px rgba(0,0,0,.15)"
+        height="250"
+        position="relative"
+        p="10px 10px 10px 30px"
+        css={{ "box-shadow": "0 0 10px rgba(0,0,0,.15)" }}
+        bg="#000"
+        borderRadius="20"
+        _before={{
+          top: "10px",
+          left: "12px",
+          position: "absolute",
+          content: '" "',
+          display: "block",
+          width: "5px",
+          height: "230px",
+          background: "white",
+          "border-radius": "5px",
+        }}
+      >
+        <Box w="100%" h="45%" borderRadius="7px" border="1px solid #dfdfdf">
+          <Box display="flex" alignItems="center" justifyContent="center">
+            <Image
+              height="100px"
+              width="120px"
+              src={imagem}
+              alt="Image"
+              display="flex"
+              alignItems="center"
+              justifyContent="center"
+            />
+          </Box>
+        </Box>
+        <Box
+          h="7%"
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+          mt="1%"
+        >
+          <Heading
+            fontSize={{ base: "10px", md: "14px", lg: "16px" }}
+            color="#FFF"
+            as="h3"
+          >
+            <LinkOverlay href="/">{name}</LinkOverlay>
+          </Heading>
+        </Box>
+        <Box h="47%" display="flex">
+          <Box fontSize={{ base: "8px", md: "10px", lg: "12px" }} color="#FFF">
+            <Box>
+              <Text>Força:</Text>
+            </Box>
+            <Box>
+              <Text>Ataque:</Text>
+            </Box>
+            <Box>
+              <Text>Defesa:</Text>
+            </Box>
+            <Box>
+              <Text>Agilidade:</Text>
+            </Box>
+          </Box>
+          <Box fontSize={{ base: "8px", md: "10px", lg: "12px" }} color="#FFF">
+            <Box>
+              <Text>{typepoke()}</Text>
+            </Box>
+            <Box>
+              <Text>{typepoke()}</Text>
+            </Box>
+          </Box>
+        </Box>
+      </LinkBox>
     </>
-  )
-}
+  );
+};
 
 export default CardPokemons;
--- Pokemon/src/pages/test.jsx
+++ Pokemon/src/pages/test.jsx
@@ -1,9 +1,82 @@
-import Conteiner from '../components/conteiner'
+import Conteiner from "../components/conteiner";
 
-export default function Test(){
-    return (
-        <>
-            <Conteiner />   
-        </>
-    )
+export default function Test() {
+  return (
+    <>
+      <Conteiner />
+      <Box
+        padding="50px 0"
+        bg={"#F3F7FC"}
+        border={"solid #D9E6EC"}
+        borderWidth="1px 0"
+      >
+        <Container position="relative" maxW={"1200px"}>
+          <Box
+            w="100%"
+            float="left"
+            position="relative"
+            _before={{
+              position: "absolute",
+              content: '" "',
+              display: "block",
+              width: "5px",
+              height: "100%",
+              background: "#272B44",
+              "border-radius": "5px",
+            }}
+          >
+            <Image
+              float="left"
+              src="./download.png"
+              alt="Super Ofertas"
+              w={"65px"}
+              h={"50px"}
+              margin="0 10px 0 20px"
+            />
+            <Heading
+              as="h2"
+              color={"#272B44"}
+              fontSize={{ base: "22px", md: "24px", lg: "26px" }}
+            >
+              Pokemons
+            </Heading>
+            <Heading
+              as="h3"
+              color={"#16428E"}
+              p={"0px"}
+              fontSize={{ base: "14px", md: "16px", lg: "18px" }}
+            >
+              1 Temporada
+            </Heading>
+          </Box>
+
+          <Box
+            position="absolute"
+            textTransform={"uppercase"}
+            top="0"
+            right="16px"
+          >
+            <Input
+              onChange={(e) => filtraPokemon(e.target.value)}
+              variant="flushed"
+              placeholder="Pesquisar Pokémon"
+              fontSize={{ base: "10px", md: "12px", lg: "24px" }}
+            />
+          </Box>
+
+          <Box sx={{ clear: "both" }} mb={"53px"}></Box>
+
+          <SimpleGrid columns="3" gap="25px" textTransform={"uppercase"}>
+            {pokemons.map((pokemon) => (
+              <CardPokemons
+                name={pokemon.data.name}
+                imagem={pokemon.data.sprites.front_default}
+                types={pokemon.data.types}
+              />
+            ))}
+          </SimpleGrid>
+        </Container>
+      </Box>
+    </>
+  );
 }
